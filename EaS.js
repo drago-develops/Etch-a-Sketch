37a3a826ee512d98fdc94c9fa51b741ac1b64939
drago-develops <ambro.rot@gmail.com>
2024-06-24 17:40:47 +0100

    container css edit
    styled container where the grid drawing will happen.

--- EaS.js
+++ EaS.js
@@ -1,9 +1,10 @@
 function noOfSquares(){
     var val = document.getElementById("gridSlider").value;
-    document.getElementById("output").innerHTML = val;
+    document.getElementById("output").innerHTML = `${val}x${val}`;
     for (i=1; i<=val; i++){
         console.log(i);
         const newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "sqr")
         document.getElementById("cointainer").appendChild(newDiv);
     }
 }
