0a93cbf9253d9b62b9bc56b7fd562824ba09742e
drago-develops <ambro.rot@gmail.com>
2024-07-01 14:02:15 +0100

    updated noOfSquares()
    added an if statement that will check if "sqr" class present and if
    so then will delete. this will help this function as when input
    changes it will deleted old grid and output only desired amount of
    squares desired by user.
    also cleaned up all files out of redundant code and/or adjusted it
    to help create the drawing grid.

--- EaS.js
+++ EaS.js
@@ -1,10 +1,17 @@
 function noOfSquares(){
+    var element =  document.getElementsByClassName("sqr");
+    if (typeof(element) != 'undefined' && element != null)
+    {   
+        document.querySelectorAll(".sqr").forEach(el=>el.remove());
+        console.log("deleted")
+    };
     var val = document.getElementById("gridSlider").value;
     document.getElementById("output").innerHTML = `${val}x${val}`;
     for (i=1; i<=val; i++){
-        console.log(i);
-        const newDiv = document.createElement("div");
-        newDiv.setAttribute("class", "sqr")
-        document.getElementById("cointainer").appendChild(newDiv);
-    }
+    console.log(i);
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "sqr")
+    document.getElementById("cointainer").appendChild(newDiv);
+    };
+    
 }
