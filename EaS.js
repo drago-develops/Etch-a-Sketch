ac99fb160623b0d314658ff7cedda37d075152f8
drago-develops <ambro.rot@gmail.com>
2024-07-04 15:11:36 +0100

    actually creating a grid
    after many attends to use css flexbox to create desired grid I have
    realised that my noOfSquares function is not creating a desired
    grid of 16x16 up to 100x100 and anything inbetween. I have added
    another loop within initial loop to fix that.
    now I will focus on using css flex box and #sqr to make each square identical
    and fill out whole drawing pad of 800px by 800px

--- EaS.js
+++ EaS.js
@@ -8,10 +8,15 @@ function noOfSquares(){
     var val = document.getElementById("gridSlider").value;
     document.getElementById("output").innerHTML = `${val}x${val}`;
     for (i=1; i<=val; i++){
-    console.log(i);
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "sqr")
-    document.getElementById("cointainer").appendChild(newDiv);
+        console.log(i);
+        const newCont = document.createElement("div");
+        newCont.setAttribute("id", `cont${i}`);
+        document.getElementById("cointainer").appendChild(newCont);
+        for (j=1; j<=val; j++){
+            const newDiv = document.createElement("div");
+            newDiv.setAttribute("class", "sqr")
+            document.getElementById(`cont${i}`).appendChild(newDiv);
+        }
     };
     
 }
