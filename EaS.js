35c644888b8765f10c150890a2e5b1f5ff5f921d
drago-develops <ambro.rot@gmail.com>
2024-07-11 20:07:23 +0100

    added function to pencil to be able to draw
    added onclick function that "assigns" eventListener to each of the
    class square with type: 'mouseover' to change background color of
    the div into black.
    so once button is selected and mouse hovers over the square div it
    will change color into black; a "stoke" of mouse will live a line or
    nice drawing for the user.

--- EaS.js
+++ EaS.js
@@ -15,7 +15,8 @@ function noOfSquares(){
         document.getElementById("cointainer").appendChild(newCont);
         for (j=1; j<=val; j++){
             const newDiv = document.createElement("div");
-            newDiv.setAttribute("class", "sqr")
+            newDiv.setAttribute("class", "sqr");
+            newDiv.setAttribute("id","square");
             document.getElementById(`cont${i}`).appendChild(newDiv);
         }
     };
@@ -41,3 +42,24 @@ buttons.forEach(button => {
     button.classList.add('active');
     });
 });
+
+// function pencilDrawing(){
+//     document.getElementById("square").addEventListener('mouseover', pencilColouring());
+//     function pencilColouring(){
+//         document.getElementById("square").style.backgroundColor = "pink";
+//     };
+
+// };
+
+// const pencilDrawing = document.getElementById("pencil")
+
+// pencilDrawing.addEventListener('click', pencilHovering);
+
+function pencilHovering(){
+    let squares = document.getElementsByClassName('sqr');
+    for(let i = 0; i < squares.length; i++){
+        squares[i].addEventListener('mouseover', function() {
+            this.style.backgroundColor = 'black';
+        });
+    }
+}
